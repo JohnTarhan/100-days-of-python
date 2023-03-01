ec21a0350ba164a83e1a98a1685ed27507ba73ba
JohnTarhan <73330678+JohnTarhan@users.noreply.github.com>
2023-03-01 17:52:58 +0300

    Delete Heads_or_Tails.py

--- Heads_or_Tails.py
+++
@@ -1,9 +0,0 @@
-#Write the rest of your code below this line 👇
-import random
-
-random_number = random.randint(0,1)
-
-if random_number == int(1):
-    print('Heads')
-if random_number == int(0):
-    print('Tails')
